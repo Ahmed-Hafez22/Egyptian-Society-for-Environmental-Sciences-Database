82d401218950ef16b3d6ce645ba215c58f863559
Ahmed-Hafez22 <ahmedmohamedhafez715@gmail.com>
2025-09-16 19:20:48 +0300

    GUI updated

--- GUI.py
+++ GUI.py
@@ -3,6 +3,15 @@ import API
 import SideFunctions
 
 
+def create_hover_functions(button_widget, hover_color, leave_color):
+    def on_enter(event=None):
+        button_widget.config(background=hover_color)
+
+    def on_leave(event=None):
+        button_widget.config(background=leave_color)
+
+    return on_enter, on_leave
+
 WIDTH = 1000
 HEIGHT = 500
 yVelocity = 30
@@ -13,16 +22,16 @@ def show_operation_menu():
         Operation_menu.place(relx=0.5, rely=0.5, anchor=CENTER)
         Operation_menu.create_text((screen_width/2), 30, text="ESES Database", font=("courier new", 30))
         Operation_menu.create_text(150, 100, text="Operations List:- ", font=("calibri", 30))
-        import_excel_file_button = Button(Operation_menu, background="#ffda7c", text="1-Import Excel File", font=("oswald", 20), borderwidth=0, activebackground="#fac84a")
-        def change_button_color_hover(event= None):
-            import_excel_file_button.config(background="#fce5aa")
-        def change_button_color_leave(event= None):
-            import_excel_file_button.config(background="#ffda7c")
-        import_excel_file_button.bind("<Enter>", change_button_color_hover)
-        import_excel_file_button.bind("<Leave>", change_button_color_leave)
+        import_excel_file_button = Button(Operation_menu, background="#ffda7c", text="1-Import Excel File", font=("oswald", 20), relief="flat", activebackground="#fac84a", command=import_excel_file)
+        hover_enter_importExcelButton, hover_leave_importExcelButton = create_hover_functions(import_excel_file_button, "#fce5aa", "#ffda7c") 
+        import_excel_file_button.bind("<Enter>", hover_enter_importExcelButton)
+        import_excel_file_button.bind("<Leave>", hover_leave_importExcelButton)
         Operation_menu.create_window(155, 160, window=import_excel_file_button)
-        import_excel_file_button.bind("<Button-1>", import_excel_file)
-
+        register_new_member = Button(Operation_menu, background="#ffda7c", text="2-Register New Member", font=("Oswald", 20), relief="flat", activebackground="#fac84a", command=regist_new_member)
+        hover_enter_registNewMemberButton, hover_leave_registNewMemberButton = create_hover_functions(register_new_member, "#fce5aa", "#ffda7c")
+        Operation_menu.create_window(190, 220, window=register_new_member)
+        register_new_member.bind("<Enter>", hover_enter_registNewMemberButton)
+        register_new_member.bind("<Leave>", hover_leave_registNewMemberButton)
         
 
 is_fading = True
@@ -120,23 +129,64 @@ def import_excel_file(event=None):
         import_excel_file_canvas.place_forget()
         Operation_menu.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    chooseFileButton = Button(window, background="#ffda7c", font=("oswald", 25), activebackground="#fac84a", text="Choose a file", command=open_excel_file)
-    returnToMainMenuButton = Button(window, background="#ffda7c", font=("oswald", 25), activebackground="#fac84a", text="Return", command=return_to_main_menu)
+    chooseFileButton = Button(window, background="#ffda7c", font=("oswald", 25), activebackground="#fac84a", text="Choose a file", command=open_excel_file, relief="groove")
+    returnToMainMenuButton = Button(window, background="#ffda7c", font=("oswald", 25), activebackground="#fac84a", text="Return", command=return_to_main_menu, relief="groove")
     import_excel_file_canvas.create_window(200, 180, window=chooseFileButton)
     import_excel_file_canvas.create_window(160, 320, window=returnToMainMenuButton)
-    def change_excelButton_color_hover(event= None):
-        chooseFileButton.config(background="#fce5aa")
-    def change_excelButton_color_leave(event= None):
-        chooseFileButton.config(background="#ffda7c")
-    chooseFileButton.bind("<Enter>", change_excelButton_color_hover)
-    chooseFileButton.bind("<Leave>", change_excelButton_color_leave)
+    hover_enter_chooseFileButton, hover_leave_chooseFileButton = create_hover_functions(chooseFileButton, "#fce5aa", "#ffda7c")
+    chooseFileButton.bind("<Enter>", hover_enter_chooseFileButton)
+    chooseFileButton.bind("<Leave>", hover_leave_chooseFileButton)
+    hover_enter_returnButton, hover_leave_returnButton = create_hover_functions(returnToMainMenuButton, "#fce5aa", "#ffda7c")
+    returnToMainMenuButton.bind("<Enter>", hover_enter_returnButton)
+    returnToMainMenuButton.bind("<Leave>", hover_leave_returnButton)
 
-    def change_returnButton_color_hover(event= None):
-        returnToMainMenuButton.config(background="#fce5aa")
-    def change_returnButton_color_leave(event= None):
-        returnToMainMenuButton.config(background="#ffda7c")
-    returnToMainMenuButton.bind("<Enter>", change_returnButton_color_hover)
-    returnToMainMenuButton.bind("<Leave>", change_returnButton_color_leave)
+
+def regist_new_member(event = None):
+
+    def submit_new_member_info(event = None):
+
+        API_URL = "http://127.0.0.1:8000/Create-Member"
+
+        new_member_dict = {
+            "member_name" : member_name_entry.get(),
+            "phone_number" : phone_numebr_entry.get(),
+            "member_email" : member_email_entry.get(),
+            "reg_date" : reg_date_entry.get()
+        }
+
+        request = requests.post(API_URL, json=new_member_dict)
+
+        if request.status_code == 200:
+            regist_new_member_canvas.create_text(400, 400, text=new_member_dict["member_name"] + " got add successfully", font=("Arial", 18))
+        else:
+            error_msg = request.text  # Get the error details from the server
+            regist_new_member_canvas.create_text(400, 400, text=new_member_dict["member_name"] + " didn't get added", font=("Arial", 18))
+
+
+    Operation_menu.place_forget()
+    regist_new_member_canvas = Canvas(window, width=screen_width, height=screen_height, background="#ffda7c")
+    regist_new_member_canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
+    regist_new_member_canvas.create_text((screen_width/2), 30, text="ESES Database", font=("courier new", 30))
+    regist_new_member_canvas.create_text(200, 100, text="Register new member:", font=("calibri", 30))
+    member_name_entry = Entry(regist_new_member_canvas, font=(15))
+    regist_new_member_canvas.create_text(150, 170, text="Member Name:", font=("Arial", 20))
+    regist_new_member_canvas.create_window(330, 170, window=member_name_entry, width=160, height=25)
+    phone_numebr_entry = Entry(regist_new_member_canvas, font=(15))
+    regist_new_member_canvas.create_text(150, 220, text="Phone Number:", font=("Arial", 20))
+    regist_new_member_canvas.create_window(330, 220, window=phone_numebr_entry, width=160, height=25)
+    reg_date_entry = Entry(regist_new_member_canvas, font=(15))
+    regist_new_member_canvas.create_text(165, 270, text="Registration Date:", font=("Arial", 20))
+    regist_new_member_canvas.create_window(360, 270, window=reg_date_entry, width=160, height=25)
+    member_email_entry = Entry(regist_new_member_canvas, font=(15))
+    regist_new_member_canvas.create_text(140, 320, text="Member Email:", font=("Arial", 20))
+    regist_new_member_canvas.create_window(360, 320, window=member_email_entry, width=250, height=25)
+    submit_button = Button(regist_new_member_canvas, text="Submit", relief="groove", font=("oswald", 15),  command=submit_new_member_info, activebackground="#fac84a", background="#ffda7c")
+    hover_enter_submitButton, hover_leave_submitButton = create_hover_functions(submit_button, "#fce5aa", "#ffda7c")
+    submit_button.bind("<Enter>", hover_enter_submitButton)
+    submit_button.bind("<Leave>", hover_leave_submitButton)
+    regist_new_member_canvas.create_window(100, 390, window=submit_button)
+
+    
     
 
 
